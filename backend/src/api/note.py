fa1b595e9910a8e9375b1f5ef6cb3084fc38b125
JonasZarnhofer <joza782@gmail.com>
2024-09-30 10:54:57 +0200

    get notes

--- backend/src/api/note.py
+++ backend/src/api/note.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import (
     Depends,
     HTTPException,
@@ -11,14 +11,15 @@ from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 
 from api.model.note import Note, NoteSchema
+from api.model.page import Page
 
 import crud.note
 
 
-router = APIRouter(prefix="/note", tags=["Note"])
+router = APIRouter(tags=["Note"])
 
 
-@router.post("", status_code=200)
+@router.post("/note", status_code=200)
 async def create_note(note: Note) -> NoteSchema:
     return crud.note.create_note(note)
 
@@ -26,7 +27,12 @@ async def create_note(note: Note) -> NoteSchema:
 @router.websocket("/ws/{note_id}")
 async def note_ws(socket: WebSocket, note_id: int):
     await socket.accept()
-    
+
     while True:
         data = await socket.receive_text()
         print(data)
+
+
+@router.get("/notes", response_model=List[NoteSchema])
+async def get_notes(page: Annotated[dict, Page] = Depends(Page)):
+    return crud.note.get_notes(page)
--- backend/src/crud/note.py
+++ backend/src/crud/note.py
@@ -1,6 +1,11 @@
 from api.model.note import Note, NoteSchema
+from api.model.page import Page
+
 from db.session import session
 from db.model import NoteDB
+
+from crud.utils.page import pageinate
+
 import os
 
 DATA_DIR = str(os.getcwd())
@@ -16,3 +21,8 @@ def create_note(note: Note) -> NoteSchema:
 
 def get_note_file(note_id: int):
     return open(DATA_DIR + str(note_id), "rw")
+
+
+def get_notes(page: Page) -> list[NoteSchema]:
+    db_notes = pageinate(session.query(NoteDB), page).all()
+    return [NoteSchema(id=note.id, user_id=0, title=note.title) for note in db_notes]
